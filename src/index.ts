a6f14a7fc0a4453bb451b3937c404a875af369ba
Ruslan Belyy <rbelyy@intruity.com>
2023-05-17 16:16:51 -0400

    added boilerplate database info

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,45 @@
-import express, {Request, Response} from "express";
-require('dotenv').config();
+import express, { Request, Response } from "express";
+import sql from "mssql";
+
+require("dotenv").config();
 const app = express();
-const PORT = process.env.PORT || '';
+const PORT = process.env.PORT || "";
+
+
+const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  server: 'localhost',
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000
+  },
+  options: {
+    encrypt: true, // for azure
+    trustServerCertificate: true // change to true for local dev / self-signed certs
+  }
+}
+
 
 app.get("/", (req: Request, res: Response) => {
+  sql
+    .connect(config)
+    .then((pool) => {
+      // Query
+
+      return pool
+        .request()
+        .input("input_parameter", sql.Int, value)
+        .query("select * from mytable where id = @input_parameter");
+    })
+    .then((result) => {
+      console.dir(result);
+    })
+    .catch((err) => {
+      // ... error checks
+    });
   res.send("Express + TypeScript Server");
 });
 
